465645c2a1d94a2a14ae602822a6c75d8e723dca
Pavel <shibaev.pp@phystech.edu>
2020-08-16 16:01:46 +0300

    Edit README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+Run app simply:
+``python3 app.py``.<br>
+If needed install dependencies:
+`pip3 install -r requirement.txt` <br>
+Enable debug by setting env var `export DEBUG=true # false` <br>
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session
 from bot import Bot
+import os
 
 
 app = Flask(__name__)
@@ -19,4 +20,8 @@ def get_bot_response():
 
 if __name__ == "__main__":
     app.secret_key = "test"
-    app.run(debug=True)
+    print(os.environ["DEBUG"])
+    if "DEBUG" not in os.environ:
+        app.run()
+    else :
+        app.run(debug=(os.environ["DEBUG"] == "true"))
